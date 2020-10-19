3a8634845588c330701b3d0584e22611e5bbcc46
Lupe <lupemaydana@gmail.com>
2020-10-19 18:36:58 -0400

    feat: Change likeIcon from class component to stateless component, it does not have a component

--- react-2018/index.js
+++ react-2018/index.js
@@ -1,14 +1,12 @@
-class LikeIcon extends React.Component {
-  render() {
-    return (
-      <div>
-        <span className="fa-stack fa-sm">
-          <i className="fa fa-circle fa-stack-2x blue-icon" />
-          <i className="fa fa-thumbs-up fa-stack-1x fa-inverse" />
-        </span>
-      </div>
-    )
-  }
+const LikeIcon = () => {
+  return (
+    <div>
+      <span className="fa-stack fa-sm">
+        <i className="fa fa-circle fa-stack-2x blue-icon" />
+        <i className="fa fa-thumbs-up fa-stack-1x fa-inverse" />
+      </span>
+    </div>
+  )
 }
 
 class Like extends React.Component {
