5fe847ede56649fbb263d48ab560ece295269fc1
Lupe <lupemaydana@gmail.com>
2020-10-19 18:22:37 -0400

    feat: Create like component

--- react-2018/index.js
+++ react-2018/index.js
@@ -1,3 +1,37 @@
+class LikeIcon extends React.Component {
+  render() {
+    return (
+      <div>
+        <span className="fa-stack fa-sm">
+          <i className="fa fa-circle fa-stack-2x blue-icon" />
+          <i className="fa fa-thumbs-up fa-stack-1x fa-inverse" />
+        </span>
+      </div>
+    )
+  }
+}
+
+class Like extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* Include the LikeIcon subcomponent within the Like component*/}
+        <LikeIcon />
+        <hr />
+        <div>
+          <button type="button">
+            <i
+              className="fa fa-thumbs-o-up fa-4 align-middle"
+              aria-hidden="true"
+            />
+            &nbsp;
+            <span className="align-middle">Like</span>
+          </button>
+        </div>
+      </div>
+    )
+  }
+}
 class Comment extends React.Component {
   render() {
     return (
@@ -15,20 +49,22 @@ class Status extends React.Component {
         <div className="card">
           <div className="card-block">
             <div className="row">
-              <div class="col-2">
-                <img src="https://avatars0.githubusercontent.com/u/15850514?s=460&v=4" class="profile-pic"></img>
+              <div className="col-2">
+                <img src="https://avatars0.githubusercontent.com/u/15850514?s=460&v=4" className="profile-pic"></img>
               </div>
               <div className="col-10 profile-row">
                 <div className="row">
                   <a href="#">Imilla loves React</a>
                 </div>
-                <div class="row">
+                <div className="row">
                   <small className="post-time">10 mins</small>
                 </div>
               </div>
+              <p>Hello world!</p>
             </div>
+            <Like />
           </div>
-          <p>Hello world!</p>
+
           <div className="card-footer text-muted">
             <Comment />
           </div>
