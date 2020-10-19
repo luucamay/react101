b30071d98376046b63e5433d7f34059e1ad393c3
Lupe <lupemaydana@gmail.com>
2020-10-19 18:16:30 -0400

    feat: Create status component

--- react-2018/index.js
+++ react-2018/index.js
@@ -1,10 +1,29 @@
-
-  class HelloWorld extends React.Component {
-    render() {
-      // Tells React what HTML code to render
-      return <h1>Hello World</h1>
-    }
+class Status extends React.Component {
+  render() {
+    return (
+      <div className="col-6 offset-3">
+        <div className="card">
+          <div className="card-block">
+            <div className="row">
+              <div class="col-2">
+                <img src="https://avatars0.githubusercontent.com/u/15850514?s=460&v=4" class="profile-pic"></img>
+              </div>
+              <div className="col-10 profile-row">
+                <div className="row">
+                  <a href="#">Imilla loves React</a>
+                </div>
+                <div class="row">
+                  <small className="post-time">10 mins</small>
+                </div>
+              </div>
+            </div>
+          </div>
+          <p>Hello world!</p>
+          <div className="card-footer text-muted" />
+        </div>
+      </div>
+    )
   }
-  
-  // Tells React to attach the HelloWorld component to the 'root' HTML div
-  ReactDOM.render(<HelloWorld />, document.getElementById("root"))
+}
+
+ReactDOM.render(<Status />, document.getElementById("root"))
