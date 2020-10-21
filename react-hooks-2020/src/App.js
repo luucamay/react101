d9e61dddc5b846456993ce8850027fe2e9c06a1a
Lupe <lupemaydana@gmail.com>
2020-10-20 23:13:11 -0400

    feat: Add color buttons, useState to change colors, handle events and jsx written

--- react-hooks-2020/src/App.js
+++ react-hooks-2020/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('')
   return (
-    <h1>Hello world!</h1>
+    <div className={`react-root ${color}`}>
+      <div className='centered'>
+        <h1>Color Picker</h1>
+        <button className='red' onClick={() => setColor('red')}>red</button>
+        <button className='blue' onClick={() => setColor('blue')}>blue</button>
+        <button className='yellow'  onClick={() => setColor('yellow')}>yellow</button>
+      </div>
+    </div>
   );
 }
 
